c0fbe7d67ee0763a402a513a7c1152708b2ae89f
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2020-01-21 15:50:34 +0000

    Project Initial Setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./main/App";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  loadTranslations,
+  setLocale,
+  syncTranslationWithStore
+} from "react-redux-i18n";
+import en from "./i18n/en";
+import pt from "./i18n/pt";
+
+import promise from "redux-promise";
+import multi from "redux-multi";
+import thunk from "redux-thunk";
+
+import reducers from "./main/reducers";
+
+// import "./components/style/custom.css";
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// create store
+const store = applyMiddleware(thunk, multi, promise)(createStore)(
+  reducers,
+  devTools
+);
+
+// add translations to Store
+export const translationsObject = {
+  en: en,
+  pt: pt
+};
+
+syncTranslationWithStore(store);
+store.dispatch(loadTranslations(translationsObject));
+store.dispatch(setLocale("en"));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("wrapper")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/main/routes.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+export default props => (
+  <BrowserRouter>
+    <div>Hello</div>
+  </BrowserRouter>
+);
