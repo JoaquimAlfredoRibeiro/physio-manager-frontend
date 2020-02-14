32c8f48d706e0cdf534c8260b3080b6d170d5583
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2020-02-14 12:14:26 +0000

    first basic calendar impl

--- src/components/apointments/Apointments.components.jsx
+++ src/components/apointments/Apointments.components.jsx
@@ -1,26 +1,139 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types';
 
 import { Translate } from 'react-redux-i18n'
+import { withStyles } from '@material-ui/core/styles';
+
+import ApointmentStyles from './Apointments.styles'
 import AuthRequired from '../common/AuthRequired'
 
+import Paper from "@material-ui/core/Paper";
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
+import {
+    Scheduler,
+    DayView,
+    WeekView,
+    MonthView,
+    Appointments,
+    AppointmentForm,
+    AppointmentTooltip,
+    ConfirmationDialog,
+    Toolbar,
+    ViewSwitcher
+} from '@devexpress/dx-react-scheduler-material-ui';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+import _ from 'lodash'
+
+const styles = ApointmentStyles;
+const I18n = require('react-redux-i18n').I18n;
+
+const appointments = [{
+    title: "Website Re-Design Plan",
+    startDate: new Date(2020, 1, 15, 9, 30),
+    endDate: new Date(2020, 1, 15, 11, 30),
+    id: 0,
+    location: "Room 1"
+},
+{
+    title: "Book Flights to San Fran for Sales Trip",
+    startDate: new Date(2020, 1, 15, 12, 0),
+    endDate: new Date(2020, 1, 15, 15, 0),
+    id: 1,
+    location: "Room 1"
+}]
+
+const theme = createMuiTheme({ palette: { type: "light", primary: blue } });
+
 class Apointments extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        var today = new Date(),
+            date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+        this.state = {
+            data: appointments,
+            currentDate: date,
+        };
+
+        this.commitChanges = this.commitChanges.bind(this);
+    }
+
+    commitChanges({ added, changed, deleted }) {
+        this.setState((state) => {
+            let { data } = state;
+            if (added) {
+                const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+                data = [...data, { id: startingAddedId, ...added }];
+            }
+            if (changed) {
+                data = data.map(appointment => (
+                    changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+            }
+            if (deleted !== undefined) {
+                data = data.filter(appointment => appointment.id !== deleted);
+            }
+            return { data };
+        });
+    }
+
     render() {
+        const { currentDate, data } = this.state;
+
         return (
-            <div>
-                <h1>
-                    <Translate value={'apointments.welcome'} />
-                </h1>
-            </div>
-        )
+            <MuiThemeProvider theme={theme}>
+                <Paper>
+                    <Scheduler
+                        data={data}
+                        maxHeight={630}
+                    >
+                        <ViewState
+                            currentDate={currentDate}
+                            defaultCurrentViewName="Week"
+                        />
+                        <EditingState
+                            onCommitChanges={this.commitChanges}
+                        />
+                        <IntegratedEditing />
+                        <DayView
+                            cellDuration={60}
+                            startDayHour={9}
+                            endDayHour={19}
+                        />
+                        <WeekView
+                            cellDuration={60}
+                            startDayHour={9}
+                            endDayHour={19}
+                        />
+                        <MonthView />
+                        <ConfirmationDialog />
+                        <Appointments />
+                        <AppointmentTooltip
+                            showOpenButton
+                            showDeleteButton
+                        />
+                        <AppointmentForm />
+                        <Toolbar />
+                        <ViewSwitcher />
+                    </Scheduler>
+                </Paper>
+            </MuiThemeProvider>
+        );
     }
 }
 
+
 const mapStateToProps = state => ({
-    // patientList: state.patients.patientList
+    // pathologiesList: state.pathologies.pathologiesList,
+    // showPathologyDialog: state.pathologies.showPathologyDialog,
+    // errors: state.errors
 })
 
-// const mapDispatchToProps = dispatch => bindActionCreators({ getAllPatients, clearData }, dispatch)
+// const mapDispatchToProps = dispatch => bindActionCreators({ createPathology, updatePathology, deletePathology, getAllPathologies, clearData, clearErrors, setShowPathologyDialog }, dispatch)
 
-export default AuthRequired(connect(mapStateToProps, null)(Apointments))
+export default AuthRequired(withRouter(connect(mapStateToProps, null)(withStyles(styles, { withTheme: true })(Apointments))))
